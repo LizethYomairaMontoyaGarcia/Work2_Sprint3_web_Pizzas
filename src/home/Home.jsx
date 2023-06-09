a8bcb768658007957da99bf08bf96b975f07bfac
LuisaLafaurie <m1m1respald0w0@gmail.com>
2023-06-09 12:22:20 -0500

    Realizando pizzas disponibles y los cupones

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import './home.scss'
+import './home.scss';
 
 export const Home = () => {
   return (
-   <>
-  <h1>Hola mundo</h1>
-   </>
+    <>
+      <section className='section__homePrincipal'>
+        <section className='section__contenidoHome'>
+          <div className='section__pizzasDisponibles'>
+            <h1>Pizzas disponibles</h1>
+            <a href="#">Ver todas</a>
+          </div>
+          <div className='section__cupones'>
+            <div className='section__scrollCupones'>
+              <button className='section__botonesCupones'>
+                <div className='section__cuponBase'>
+                <p>Cupón para Frontends</p>
+                <h1>45% OFF</h1>
+                </div>
+              </button>
+
+              <button className='section__botonesCupones'>
+              <div className='section__cuponBase'>
+                <p>Cupón Pizza CSS</p>
+                <h1>25% OFF</h1>
+                </div>
+              </button>
+
+              <button className='section__botonesCupones'>
+              <div className='section__cuponBase'>
+                <p>Cupón para Frontends</p>
+                <h1>45% OFF</h1>
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className='section__cardsPizzas'>
+
+          </div>
+        </section>
+        <section>
+        </section>
+      </section>
+
+    </>
   )
 }
