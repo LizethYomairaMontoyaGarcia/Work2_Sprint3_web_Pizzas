c6f0045b933870cd07b270c7a4e4cb9ffb252523
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-06-10 10:40:31 -0500

    creando el formulario de pago, falta mapear los agregados al carrito

--- src/shoppingCart/ShoppingTrolley.jsx
+++ src/shoppingCart/ShoppingTrolley.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
-import CalendarExit from "./Calendar";
+import Flecha from "../images/flecha.png";
+import "./shoppingTrolley.scss";
 
 const validationSchema = Yup.object({
   fullName: Yup.string().required("El nombre completo es requerido"),
@@ -26,50 +27,107 @@ const ShoppingTrolley = () => {
   const onSubmit = (values) => {
     console.log(values);
     sessionStorage.setItem("paymentInfo", JSON.stringify(values));
-    navigate("pago");
+    navigate("/successFull");
   };
 
   return (
-    <div>
-      <h1>Carrito de compras</h1>
-
+    <div className="shoppingTrolley">
       <div>
-        <h3>Información de pago</h3>
+        <div className="shoppingTrolley__titleImgFlech">
+          <img src={Flecha} alt="flecha" />
+          <h1>Carrito de compras</h1>
+        </div>
+
+        <h3 className="shoppingTrolley__info">Información de pago</h3>
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          <Form>
+            <div>
+              <label htmlFor="fullName" className="shoppingTrolley__label">
+                Nombre Completo
+              </label>
+              <Field
+                type="text"
+                id="fullName"
+                name="fullName"
+                placeholder="Ingresa tu nombre"
+                className="shoppingTrolley__input"
+              />
+              <ErrorMessage
+                name="fullName"
+                component="div"
+                className="shoppingTrolley__x"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="creditCard" className="shoppingTrolley__label">
+                Número de Tarjeta de crédito
+              </label>
+              <Field
+                type="text"
+                id="creditCard"
+                name="creditCard"
+                placeholder="1234562587"
+                className="shoppingTrolley__input"
+              />
+              <ErrorMessage
+                name="creditCard"
+                component="div"
+                className="shoppingTrolley__x"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="expirationDate"
+                className="shoppingTrolley__label"
+              >
+                Fecha de Vencimiento
+              </label>
+              <Field
+                type="text"
+                id="expirationDate"
+                name="expirationDate"
+                placeholder="DD/MM/AA"
+                className="shoppingTrolley__input"
+              />
+
+              <ErrorMessage
+                name="expirationDate"
+                component="div"
+                className="shoppingTrolley__x"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="address" className="shoppingTrolley__label">
+                Dirección
+              </label>
+              <Field
+                as="textarea"
+                id="address"
+                name="address"
+                placeholder="Av.morelos#123"
+                className="shoppingTrolley__input"
+              />
+              <ErrorMessage
+                name="address"
+                component="div"
+                className="shoppingTrolley__x"
+              />
+            </div>
+
+            <button type="submit" className="shoppingTrolley__submit">
+              Pagar Ahora
+            </button>
+          </Form>
+        </Formik>
       </div>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        <Form>
-          <div>
-            <label htmlFor="fullName">Nombre Completo</label>
-            <Field type="text" id="fullName" name="fullName" />
-            <ErrorMessage name="fullName" component="div" />
-          </div>
-
-          <div>
-            <label htmlFor="creditCard">Número de Tarjeta de crédito</label>
-            <Field type="text" id="creditCard" name="creditCard" />
-            <ErrorMessage name="creditCard" component="div" />
-          </div>
-
-          <div>
-            <label htmlFor="expirationDate">Fecha de Vencimiento</label>
-            <Field type="text" id="expirationDate" name="expirationDate" />
-            <CalendarExit />
-            <ErrorMessage name="expirationDate" component="div" />
-          </div>
-
-          <div>
-            <label htmlFor="address">Dirección</label>
-            <Field as="textarea" id="address" name="address" />
-            <ErrorMessage name="address" component="div" />
-          </div>
-
-          <button type="submit">Pagar Ahora</button>
-        </Form>
-      </Formik>
     </div>
   );
 };
